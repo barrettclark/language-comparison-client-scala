b5d5f03ac81c3bd16b6cb5dc7994ba0588cf969f
Jeffrey N. Davis <Jeffrey.N.Davis@gmail.com>
2015-07-14 12:13:48 -0500

    Replaced Util method with Function<1> literal

--- src/main/scala/util.scala
+++ src/main/scala/util.scala
@@ -4,7 +4,7 @@ import com.twitter.io.Buf
 import com.twitter.io.Buf.Utf8
 
 object Util {
-  def bufToString(buf: Buf): String = Utf8.unapply(buf) match {
+  val bufToString = (buf: Buf) => Utf8.unapply(buf) match {
     case Some(x) => x
     case None    => ""
   }
